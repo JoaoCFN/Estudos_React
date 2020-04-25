15d9bb53be92f90bdbfddda958e970d35f3e3256
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-25 00:44:07 -0300

    Update App.js

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -4,7 +4,6 @@ import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Sobre from "./Pages/Sobre";
 import Produtos from "./Pages/Produtos";
-import "./estilo.css";
 
 const App = () => {
     /* Para trabalharmos com rotas precisamos importar a lib react router dom
