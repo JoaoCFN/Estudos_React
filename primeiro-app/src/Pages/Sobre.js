3c114833b92f405faac23ea732df78553abace41
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-24 23:50:33 -0300

    Update Sobre.js

--- primeiro-app/src/Pages/Sobre.js
+++ primeiro-app/src/Pages/Sobre.js
@@ -1,20 +1,51 @@
 import React, { useState } from 'react'
 
+// 1 º MANEIRA
+// const Sobre = () => {
+//     const [nome, setNome] = useState("João");
+//     const [email, setEmail] = useState("joaoneto.10");
+
+//     return (
+//         <div>
+//             <h1>Sobre</h1>
+//             <form action="">
+//                 <label htmlFor="nome">{nome}</label>
+//                 <input type="text" onChange={(event) => setNome(event.target.value)}/>
+//                 <label htmlFor="email">{email}</label>
+//                 <input type="email"onChange={(event) => setEmail(event.target.value)}/>
+//             </form>
+//         </div>
+//     )
+// }
+
 const Sobre = () => {
-    const [nome, setNome] = useState("João");
-    const [email, setEmail] = useState("joaoneto.10");
+    const [form, setForm] = useState({
+        nome: "João",
+        email: "joaoneto.10"
+    });
+
+    const atualizaCampo = (event) => {
+        setForm({
+            /* Desestruturei o conteúdo de form */
+            ...form,
+            /* pego o id do input que corresponde a um item dentro de form e jogo 
+               o valor dentro */
+            [event.target.id] : event.target.value
+        })
+        
+    }
 
     return (
         <div>
             <h1>Sobre</h1>
             <form action="">
-                <label htmlFor="nome">{nome}</label>
-                <input type="text" onChange={(event) => setNome(event.target.value)}/>
-                <label htmlFor="email">{email}</label>
-                <input type="email"onChange={(event) => setEmail(event.target.value)}/>
+                <label htmlFor="nome">{form.nome}</label>
+                <input type="text" id="nome" onChange={atualizaCampo}/>
+                <label htmlFor="email">{form.email}</label>
+                <input type="email" id="email" onChange={atualizaCampo}/>
             </form>
         </div>
-    )
+    )  
 }
 
 export default Sobre
