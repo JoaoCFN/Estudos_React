35ecff2b0ff77bcf7d5bfd97987cb93cd1fca827
João Carlos Ferreira Neto <58020444+JoaoCFN@users.noreply.github.com>
2020-04-28 15:44:00 -0300

    Finalização do bg dinâmico

--- bg-dinamico/src/App.js
+++ bg-dinamico/src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
-import './App.css'
+import './App.css';
 
 function App() {
   const [color, Setcolor] = useState("#000000");
-  document.body.style.backgroundColor = color;
+  const colors = { 
+    black: "#000000",
+    blue: "#3297bf",
+    green: "#3d8025"
+  }
+
+  const body = document.querySelector("body");
+  body.style.backgroundColor = color;
+  body.style.transition = `1s`;
 
   return (
     <div className="App">
-      <button className = "btn btn-black">Black</button>
-      <button className = "btn btn-blue">Blue</button>
-      <button className = "btn btn-green">Greeen</button>
+      <h1 className="titulo">Background Dinâmico</h1>    
+      <button className = "btn btn-black" onClick={() => Setcolor(colors.black)}>Black</button>
+      <button className = "btn btn-blue" onClick={() => Setcolor(colors.blue)}>Blue</button>
+      <button className = "btn btn-green" onClick={() => Setcolor(colors.green)}>Greeen</button>
     </div>
   );
 }
